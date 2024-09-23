6034ca280a3c8941ce7e108848e7b8c425954554
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-23 10:28:20 +0400

    Declearing Dictionary for course and list for waitlist, adding main menue.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,85 @@
 {
     internal class Program
     {
+        static Dictionary<string, HashSet<string>> Courses = new Dictionary<string, HashSet<string>>(10);
+        static List<(string,string)> waitlist = new List<(string,string)> ();
         static void Main(string[] args)
         {
-            
+            bool flge = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\t****************Course Enrollment System****************");
+                Console.WriteLine("---------------------------------------------------------------\n");
+                Console.WriteLine("Enter the number of your choice \n");
+                Console.WriteLine(" 1.Add a new course\n");
+                Console.WriteLine(" 2.Remove Course\n");
+                Console.WriteLine(" 3.Enroll a student in a course\n");
+                Console.WriteLine(" 4.Remove a student from a course\n");
+                Console.WriteLine(" 5.Display all students in a course\n");
+                Console.WriteLine(" 6.Display all courses and their students\n");
+                Console.WriteLine(" 7.Find courses with common students\n");
+                Console.WriteLine(" 8.Withdraw a Student from All Courses\n");
+                int choice = handelIntError(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input..");
+                        break;
+                }
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("\nPress 1 if you want to Enter another choise..");
+                string cont = Console.ReadLine();
+                if(cont != "1")
+                {
+                    Console.WriteLine("Thank You");
+                    flge = false;
+                }
+                else
+                    flge = true;
+
+            } while (flge== true);
+
+        }
+        static int handelIntError(string input)
+        {
+            int num;
+            bool flag = true;
+            do
+            {
+                if (int.TryParse(input, out num))
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("re-enter input");
+                    input = Console.ReadLine();
+                    flag = true;
+                }
+            } while (flag == true);
+            return num;
         }
     }
 }
